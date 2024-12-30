de5d1df380e5f1c5d5cbe000b7677d0e4501f4ce
Maarten Mulders <m.th.mulders@xs4all.nl>
2024-12-30 22:53:03 +0100

    chore: Define correct component model for MapStruct mappers

--- app/adapters/api/src/main/java/it/mulders/traqqr/api/measurements/MeasurementMapper.java
+++ app/adapters/api/src/main/java/it/mulders/traqqr/api/measurements/MeasurementMapper.java
@@ -5,8 +5,9 @@ import it.mulders.traqqr.domain.measurements.Measurement;
 import it.mulders.traqqr.domain.vehicles.Vehicle;
 
 import org.mapstruct.Mapper;
+import org.mapstruct.MappingConstants;
 
-@Mapper(componentModel = "jakarta")
+@Mapper(componentModel = MappingConstants.ComponentModel.JAKARTA_CDI)
 public interface MeasurementMapper {
     Measurement toMeasurement(Vehicle vehicle, MeasurementDto measurementDto);
 }
