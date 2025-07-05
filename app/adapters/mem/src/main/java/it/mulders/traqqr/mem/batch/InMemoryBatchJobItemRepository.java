25c9b1cc817842cb7df17cdf46f44f2f0c5e09e6
Maarten Mulders <m.th.mulders@xs4all.nl>
2025-07-05 22:27:27 +0200

    test: Improve test coverage

---
+++ app/adapters/mem/src/main/java/it/mulders/traqqr/mem/batch/InMemoryBatchJobItemRepository.java
@@ -0,0 +1,32 @@
+package it.mulders.traqqr.mem.batch;
+
+import it.mulders.traqqr.domain.batch.BatchJobItem;
+import it.mulders.traqqr.domain.batch.BatchJobItemRepository;
+import it.mulders.traqqr.domain.batch.BatchJobItemStatus;
+import java.util.Collection;
+import java.util.HashSet;
+import java.util.Map;
+import java.util.Set;
+
+public class InMemoryBatchJobItemRepository implements BatchJobItemRepository {
+    private final Set<BatchJobItem<?>> batchJobItems = new HashSet<>();
+
+    @Override
+    public Map<BatchJobItemStatus, Long> findItemCountsForJobInstanceAndExecution(Long instanceId, Long executionId) {
+        throw new UnsupportedOperationException("Not supported yet.");
+    }
+
+    @Override
+    public void save(BatchJobItem<?> item) {
+        batchJobItems.add(item);
+    }
+
+    @Override
+    public void saveAll(Collection<BatchJobItem<?>> items) {
+        batchJobItems.addAll(items);
+    }
+
+    public Set<BatchJobItem<?>> getBatchJobItems() {
+        return batchJobItems;
+    }
+}
