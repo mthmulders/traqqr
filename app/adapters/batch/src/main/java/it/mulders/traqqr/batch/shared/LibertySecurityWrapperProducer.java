f36874e8489a24ee1e98c8b805e0907fac7bf52a
Maarten Mulders <m.th.mulders@xs4all.nl>
2025-09-03 17:53:13 +0200

    feat: Externalise password for Scheduler NPA

--- app/adapters/batch/src/main/java/it/mulders/traqqr/batch/shared/LibertySecurityWrapperProducer.java
+++ app/adapters/batch/src/main/java/it/mulders/traqqr/batch/shared/LibertySecurityWrapperProducer.java
@@ -4,12 +4,23 @@ import it.mulders.traqqr.libertysecurity.LibertySecurityWrapper;
 import it.mulders.traqqr.libertysecurity.SecurityWrapper;
 import jakarta.enterprise.context.ApplicationScoped;
 import jakarta.enterprise.inject.Produces;
+import org.slf4j.Logger;
+import org.slf4j.LoggerFactory;
 
 @ApplicationScoped
 public class LibertySecurityWrapperProducer {
+    private static final Logger logger = LoggerFactory.getLogger(LibertySecurityWrapperProducer.class);
+
     @ApplicationScoped
     @Produces
     public SecurityWrapper wrapper() {
-        return new LibertySecurityWrapper("scheduler", "correct-horse-battery-staple");
+        var password = System.getenv("SCHEDULER_USER_PASSWORD");
+        if (password == null || password.isEmpty()) {
+            logger.warn("Configuring LibertySecurityWrapper; environment variable SCHEDULER_USER_PASSWORD not set");
+        } else {
+            logger.info("Configuring LibertySecurityWrapper; using password from environment variable");
+        }
+
+        return new LibertySecurityWrapper("scheduler", password);
     }
 }
