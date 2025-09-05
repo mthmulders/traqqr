1c81e6cb0c7aedac320e736d085fb53babdd5067
Maarten Mulders <m.th.mulders@xs4all.nl>
2025-09-05 17:14:30 +0200

    test: Improve test coverage

--- app/adapters/batch/src/test/java/it/mulders/traqqr/batch/example/ExampleJobStarterTest.java
+++ app/adapters/batch/src/test/java/it/mulders/traqqr/batch/example/ExampleJobStarterTest.java
@@ -1,20 +1,47 @@
 package it.mulders.traqqr.batch.example;
 
 import it.mulders.traqqr.batch.jakarta.DummyJobOperator;
+import it.mulders.traqqr.domain.batch.BatchJobType;
+import it.mulders.traqqr.domain.batch.JobStartRequestedEvent;
+import it.mulders.traqqr.libertysecurity.SecurityWrapper;
 import org.assertj.core.api.WithAssertions;
 import org.junit.jupiter.api.DisplayNameGeneration;
 import org.junit.jupiter.api.DisplayNameGenerator;
 import org.junit.jupiter.api.Test;
 
+import java.security.PrivilegedAction;
+
 @DisplayNameGeneration(DisplayNameGenerator.ReplaceUnderscores.class)
 class ExampleJobStarterTest implements WithAssertions {
     private final DummyJobOperator jobOperator = new DummyJobOperator();
-    private final ExampleJobStarter job = new ExampleJobStarter(jobOperator, null);
+    private final SecurityWrapper wrapper = new SecurityWrapper() {
+        @Override
+        public <T> T execute(PrivilegedAction<T> action) {
+            return action.run();
+        }
+    };
+    private final ExampleJobStarter job = new ExampleJobStarter(jobOperator, wrapper);
 
     @Test
-    void should_start_job() {
-        job.doStart();
+    void should_start_job_manually() {
+        // Arrange
+        var event = new JobStartRequestedEvent(BatchJobType.EXAMPLE);
 
+        // Act
+        job.startManual(event);
+
+        // Assert
+        assertThat(this.jobOperator.getRequestedJobStarts()).anyMatch(rjs -> "example".equals(rjs.jobXMLName()));
+    }
+
+    @Test
+    void should_start_job_automatically() {
+        // Arrange
+
+        // Act
+        job.startAutomatic();
+
+        // Assert
         assertThat(this.jobOperator.getRequestedJobStarts()).anyMatch(rjs -> "example".equals(rjs.jobXMLName()));
     }
 }
