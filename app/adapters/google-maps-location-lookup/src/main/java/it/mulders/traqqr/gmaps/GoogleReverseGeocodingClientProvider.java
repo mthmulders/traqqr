38513fb99f8e95d775ccb229e3aa61b3a2f02757
Maarten Mulders <m.th.mulders@xs4all.nl>
2025-11-09 15:59:22 +0100

    feat: Make GoogleReverseGeocodingClient injectable

---
+++ app/adapters/google-maps-location-lookup/src/main/java/it/mulders/traqqr/gmaps/GoogleReverseGeocodingClientProvider.java
@@ -0,0 +1,25 @@
+package it.mulders.traqqr.gmaps;
+
+import jakarta.enterprise.context.ApplicationScoped;
+import jakarta.enterprise.inject.Produces;
+
+import java.util.Map;
+
+@ApplicationScoped
+public class GoogleReverseGeocodingClientProvider {
+    private final Map<String, String> environment;
+
+    public GoogleReverseGeocodingClientProvider() {
+        this(System.getenv());
+    }
+
+    public GoogleReverseGeocodingClientProvider(Map<String, String> environment) {
+        this.environment = environment;
+    }
+
+    @Produces
+    public GoogleReverseGeocodingClient createGoogleReverseGeocodingClient() {
+        var apiKey = this.environment.get("GOOGLE_REVERSE_GEOCODING_API_KEY");
+        return new GoogleReverseGeocodingClient(apiKey);
+    }
+}
