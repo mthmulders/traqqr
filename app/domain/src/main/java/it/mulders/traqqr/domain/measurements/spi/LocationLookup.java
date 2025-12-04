964f22b4805e9a4490b3e0cea7c8099a1bfa2585
Maarten Mulders <m.th.mulders@xs4all.nl>
2025-12-04 22:16:19 +0100

    chore: Reformat

--- app/domain/src/main/java/it/mulders/traqqr/domain/measurements/spi/LocationLookup.java
+++ app/domain/src/main/java/it/mulders/traqqr/domain/measurements/spi/LocationLookup.java
@@ -21,7 +21,8 @@ public interface LocationLookup {
      * <p>
      * Implementations are limited to {@link Success} and {@link Failure}.
      */
-    sealed interface LocationLookupResult permits LocationLookupResult.Success, LocationLookupResult.NotFound, LocationLookupResult.Failure {
+    sealed interface LocationLookupResult
+            permits LocationLookupResult.Success, LocationLookupResult.NotFound, LocationLookupResult.Failure {
         /** Successful lookup containing the resolved location. */
         record Success(Measurement.Location location) implements LocationLookupResult {}
 
