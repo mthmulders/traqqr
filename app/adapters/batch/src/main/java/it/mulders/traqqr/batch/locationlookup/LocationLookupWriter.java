e953f06b2c76285b3752497793f6fbe4e3d29353
Maarten Mulders <m.th.mulders@xs4all.nl>
2025-12-19 19:24:54 +0100

    feat: Write items back to database

---
+++ app/adapters/batch/src/main/java/it/mulders/traqqr/batch/locationlookup/LocationLookupWriter.java
@@ -0,0 +1,47 @@
+package it.mulders.traqqr.batch.locationlookup;
+
+import it.mulders.traqqr.domain.batch.BatchJobItem;
+import it.mulders.traqqr.domain.batch.spi.BatchJobItemRepository;
+import jakarta.batch.api.chunk.AbstractItemWriter;
+import jakarta.enterprise.context.Dependent;
+import jakarta.inject.Inject;
+import jakarta.inject.Named;
+import jakarta.transaction.Transactional;
+import org.slf4j.Logger;
+import org.slf4j.LoggerFactory;
+
+import java.util.ArrayList;
+import java.util.List;
+
+@Dependent
+@Named("locationLookupWriter")
+public class LocationLookupWriter extends AbstractItemWriter {
+    private static final Logger log = LoggerFactory.getLogger(LocationLookupWriter.class);
+
+    // Components
+    private final BatchJobItemRepository batchJobItemRepository;
+
+    @Inject
+    public LocationLookupWriter(final BatchJobItemRepository batchJobItemRepository) {
+        this.batchJobItemRepository = batchJobItemRepository;
+    }
+
+    @Override
+    @Transactional(Transactional.TxType.REQUIRES_NEW)
+    public void writeItems(List<Object> items) {
+        // The items are the individual return values of ExampleProcessor#processItem
+        var result = new ArrayList<BatchJobItem<?>>();
+
+        // Not using the Streams API because it has trouble inferring the type
+        // of the Collector to use.
+        items.forEach(item -> {
+            if (item instanceof BatchJobItem<?> batchJobItem) {
+                result.add(batchJobItem);
+            } else {
+                log.warn("Unexpected item type {}", item.getClass());
+            }
+        });
+
+        batchJobItemRepository.saveAll(result);
+    }
+}
---
+++ app/adapters/batch/src/test/java/it/mulders/traqqr/batch/locationlookup/LocationLookupWriterTest.java
@@ -0,0 +1,45 @@
+package it.mulders.traqqr.batch.locationlookup;
+
+import it.mulders.traqqr.domain.batch.BatchJob;
+import it.mulders.traqqr.domain.batch.BatchJobItem;
+import it.mulders.traqqr.domain.batch.BatchJobItemStatus;
+import it.mulders.traqqr.domain.batch.BatchJobStatus;
+import it.mulders.traqqr.domain.batch.BatchJobType;
+import it.mulders.traqqr.mem.batch.InMemoryBatchJobItemRepository;
+import org.assertj.core.api.WithAssertions;
+import org.junit.jupiter.api.DisplayNameGeneration;
+import org.junit.jupiter.api.DisplayNameGenerator;
+import org.junit.jupiter.api.Test;
+
+import java.time.OffsetDateTime;
+import java.util.List;
+import java.util.Map;
+
+import static it.mulders.traqqr.domain.fakes.MeasurementFaker.createMeasurement;
+import static it.mulders.traqqr.domain.fakes.VehicleFaker.createVehicle;
+
+@DisplayNameGeneration(DisplayNameGenerator.ReplaceUnderscores.class)
+class LocationLookupWriterTest implements WithAssertions {
+    private final InMemoryBatchJobItemRepository repository = new InMemoryBatchJobItemRepository();
+    private final LocationLookupWriter exampleWriter = new LocationLookupWriter(repository);
+
+    @Test
+    void should_store_items_in_repository() {
+        var now = OffsetDateTime.now();
+        var batchJob = new BatchJob(
+                now.minusMinutes(1),
+                now,
+                now,
+                BatchJobType.EXAMPLE,
+                BatchJobStatus.COMPLETED,
+                Map.of(BatchJobItemStatus.PROCESSED, 1L),
+                1L,
+                1L);
+        var measurement = createMeasurement(createVehicle());
+
+        var item = new BatchJobItem<>(batchJob, BatchJobItemStatus.PROCESSED, measurement);
+        exampleWriter.writeItems(List.of(item));
+
+        assertThat(repository.getBatchJobItems()).hasSize(1).contains(item);
+    }
+}
