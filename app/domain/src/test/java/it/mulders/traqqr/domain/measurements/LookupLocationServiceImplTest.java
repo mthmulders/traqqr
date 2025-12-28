05b1c12a7633e836e61f02c3fc127383854e831c
Maarten Mulders <m.th.mulders@xs4all.nl>
2025-12-28 10:58:44 +0100

    test: Improve test coverage

--- app/domain/src/test/java/it/mulders/traqqr/domain/measurements/LookupLocationServiceImplTest.java
+++ app/domain/src/test/java/it/mulders/traqqr/domain/measurements/LookupLocationServiceImplTest.java
@@ -14,10 +14,24 @@ import org.junit.jupiter.api.Test;
 
 @DisplayNameGeneration(DisplayNameGenerator.ReplaceUnderscores.class)
 class LookupLocationServiceImplTest implements WithAssertions {
+    private static final Location NULL_DESCRIPTION_LOCATION = new Location(52.320418, 4.7685652, null);
+    private static final Location EMPTY_DESCRIPTION_LOCATION = new Location(52.320418, 4.7685652, "");
+    private static final Location FILLED_DESCRIPTION_LOCATION =
+            new Location(52.320418, 4.7685652, "Wateringen, Netherlands");
+    private static final Location NOT_FOUND_LOCATION = new Location(52.382041, 4.8565276, null);
+
     private final LocationLookup lookup = new LocationLookup() {
         @Override
         public LocationLookupResult lookup(Location location) {
-            return new LocationLookupResult.Success(location);
+            if (NULL_DESCRIPTION_LOCATION.equals(location)
+                    || EMPTY_DESCRIPTION_LOCATION.equals(location)
+                    || FILLED_DESCRIPTION_LOCATION.equals(location)) {
+                return new LocationLookupResult.Success(location.withDescription("example description"));
+            } else if (NOT_FOUND_LOCATION.equals(location)) {
+                return new LocationLookupResult.NotFound();
+            } else {
+                return new LocationLookupResult.Failure(new RuntimeException("Lookup failed"));
+            }
         }
     };
     private final LookupLocationService service = new LookupLocationServiceImpl(lookup);
@@ -37,10 +51,21 @@ class LookupLocationServiceImplTest implements WithAssertions {
         }
 
         @Test
-        void should_not_lookup_if_location_description_is_null() {
+        void should_lookup_if_location_description_is_null() {
             // Arrange
-            var location = new Location(52.320418, 4.7685652, null);
-            var measurement = createMeasurement(OffsetDateTime.now(), location);
+            var measurement = createMeasurement(OffsetDateTime.now(), NULL_DESCRIPTION_LOCATION);
+
+            // Act
+            var result = service.lookupLocation(measurement);
+
+            // Assert
+            assertThat(result).isInstanceOf(LookupLocationOutcome.Success.class);
+        }
+
+        @Test
+        void should_lookup_if_location_description_is_empty() {
+            // Arrange
+            var measurement = createMeasurement(OffsetDateTime.now(), EMPTY_DESCRIPTION_LOCATION);
 
             // Act
             var result = service.lookupLocation(measurement);
@@ -52,8 +77,7 @@ class LookupLocationServiceImplTest implements WithAssertions {
         @Test
         void should_not_lookup_if_description_already_filled() {
             // Arrange
-            var location = new Location(52.320418, 4.7685652, "Wateringen, Netherlands");
-            var measurement = createMeasurement(OffsetDateTime.now(), location);
+            var measurement = createMeasurement(OffsetDateTime.now(), FILLED_DESCRIPTION_LOCATION);
 
             // Act
             var result = service.lookupLocation(measurement);
@@ -80,14 +104,41 @@ class LookupLocationServiceImplTest implements WithAssertions {
         @Test
         void should_lookup_even_if_description_already_filled() {
             // Arrange
-            var location = new Location(52.320418, 4.7685652, "Wateringen, Netherlands");
-            var measurement = createMeasurement(OffsetDateTime.now(), location);
+            var measurement = createMeasurement(OffsetDateTime.now(), FILLED_DESCRIPTION_LOCATION);
 
             // Act
             var result = service.refreshLocation(measurement);
 
             // Assert
             assertThat(result).isInstanceOf(LookupLocationOutcome.Success.class);
+        }
+    }
+
+    @Nested
+    class ResultMapping {
+        @Test
+        void should_map_NotFound_result() {
+            // Arrange
+            var measurement = createMeasurement(OffsetDateTime.now(), NOT_FOUND_LOCATION);
+
+            // Act
+            var result = service.refreshLocation(measurement);
+
+            // Assert
+            assertThat(result).isEqualTo(LookupLocationOutcome.NOT_FOUND);
+        }
+
+        @Test
+        void should_map_Failure_result() {
+            // Arrange
+            var unknownLocation = new Location(0.0, 0.0, null);
+            var measurement = createMeasurement(OffsetDateTime.now(), unknownLocation);
+
+            // Act
+            var result = service.refreshLocation(measurement);
+
+            // Assert
+            assertThat(result).isInstanceOf(LookupLocationOutcome.Failure.class);
         }
     }
 
