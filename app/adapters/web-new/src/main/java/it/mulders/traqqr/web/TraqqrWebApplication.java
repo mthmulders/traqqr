bbf18fd1ce86d6520bdad581a86e8ef4e7cefe4d
Maarten Mulders <m.th.mulders@xs4all.nl>
2026-02-20 21:25:13 +0100

    feat: Rebuild landing page with Jakarta MVC

---
+++ app/adapters/web-new/src/main/java/it/mulders/traqqr/web/TraqqrWebApplication.java
@@ -0,0 +1,8 @@
+package it.mulders.traqqr.web;
+
+import jakarta.ws.rs.ApplicationPath;
+import jakarta.ws.rs.core.Application;
+
+@ApplicationPath("/app")
+public class TraqqrWebApplication extends Application {
+}
