953abab18cfac5e8bc8462a253ee7f9019aafadf
Maarten Mulders <m.th.mulders@xs4all.nl>
2026-02-22 20:31:05 +0100

    refactor: Reduce chores of testing MVC pages

--- app/adapters/web-new/src/test/java/it/mulders/traqqr/web/vehicles/VehicleListPageTest.java
+++ app/adapters/web-new/src/test/java/it/mulders/traqqr/web/vehicles/VehicleListPageTest.java
@@ -5,18 +5,13 @@ import it.mulders.traqqr.domain.shared.RandomStringUtils;
 import it.mulders.traqqr.domain.user.Owner;
 import it.mulders.traqqr.domain.vehicles.spi.VehicleRepository;
 import it.mulders.traqqr.mem.vehicles.InMemoryVehicleRepository;
-import jakarta.mvc.Models;
-import jakarta.ws.rs.core.Response;
-import org.assertj.core.api.WithAssertions;
-import org.eclipse.krazo.core.ModelsImpl;
+import it.mulders.traqqr.web.AbstractMvcPageTest;
 import org.junit.jupiter.api.DisplayNameGeneration;
 import org.junit.jupiter.api.DisplayNameGenerator;
 import org.junit.jupiter.api.Test;
 
 @DisplayNameGeneration(DisplayNameGenerator.ReplaceUnderscores.class)
-class VehicleListPageTest implements WithAssertions {
-    private final Models models = new ModelsImpl();
-    private final Owner owner = createOwner();
+class VehicleListPageTest extends AbstractMvcPageTest {
     private final VehicleViewMapper vehicleViewMapper = new VehicleViewMapperImpl();
     private final VehicleRepository vehicleRepository = new InMemoryVehicleRepository();
 
@@ -28,8 +23,7 @@ class VehicleListPageTest implements WithAssertions {
         var response = page.show();
 
         // Assert
-        assertThat(response.getStatus()).isEqualTo(Response.Status.OK.getStatusCode());
-        assertThat(response.readEntity(String.class)).isEqualTo("vehicles/list.jsp");
+        assertThat(response).hasStatus(200).hasViewName("vehicles/list.jsp");
     }
 
     @Test
